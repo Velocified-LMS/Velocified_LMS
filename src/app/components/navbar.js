fed9f74be739ec5596baf02c49a1506bfc395e76
Sidharth-Sivaprasad <sksivapr@asu.edu>
2023-09-22 17:43:28 -0700

    Homepage stylised #27

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,5 +1,6 @@
 
 "use client"
+"use client"
 import { TextField, InputAdornment } from "@mui/material";
 import Link from "next/link";
 import styles from "./navbar.module.css";
@@ -7,13 +8,20 @@ import { Search } from "@mui/icons-material";
 import { usePathname } from "next/navigation";
 
 
+import { usePathname } from "next/navigation";
+
+
 
 const Navbar = () => {
     const router = usePathname();
 
+    const router = usePathname();
+
   return (
     
+    
     <div className={styles.header}>
+        <div className={styles.titles}><img src="/velocified-symbol.svg" alt="Velocified Logo" className={styles.velocifiedicon}/><img src="/text-only-white-background.png" alt="Velocified Text Logo" className={styles.velocifiedtext} /></div>
         <div className={styles.titles}><img src="/velocified-symbol.svg" alt="Velocified Logo" className={styles.velocifiedicon}/><img src="/text-only-white-background.png" alt="Velocified Text Logo" className={styles.velocifiedtext} /></div>
         <div className={styles.search}>
             <TextField
@@ -38,10 +46,10 @@ const Navbar = () => {
         
         <div className={styles.navlinks} >
             <Link href="/" ><div className={`${styles.home1} ${router === "/" ? styles.activeLink : ""}`}>Home </div> </Link>
-            <Link href="/about" ><div className={`${styles.about} ${router === "/about" ? styles.activeLink : ""}`}>About</div></Link>
-            <Link href="/training" ><div className={`${styles.training} ${router === "/training" ? styles.activeLink : ""}`}>Training</div></Link>
-            <Link href="/news" ><div className={`${styles.news} ${router === "/news" ? styles.activeLink : ""}`}>News</div></Link>
-            <Link href="/fr" ><div className={`${styles.fr} ${router === "/fr" ? styles.activeLink : ""}`}>FR</div></Link>
+            <Link href="/" ><div className={styles.about}>About</div></Link>
+            <Link href="/" ><div className={styles.training}>Training</div></Link>
+            <Link href="/" ><div className={styles.news}>News</div></Link>
+            <Link href="/" ><div className={styles.fr}>FR</div></Link>
             <Link href="/login" ><div className={`${styles.logIn} ${router === "/login" ? styles.activeLink : ""}`}>Log in</div></Link>
         </div>
       </div>
