8adb8491f44d114b760a7d8be142215fcbbdf39d
Vardhan Reddy <vardhan@Vardhans-MacBook-Pro.local>
2023-09-25 19:58:28 -0700

    added sidebar

---
+++ src/app/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./sidebar.module.css"
+
+const SideBar = (Content) => {
+    return (
+        <div className={styles.sidebarContainer}>
+            Hi
+        </div>
+    );
+}
+
+export default SideBar;
