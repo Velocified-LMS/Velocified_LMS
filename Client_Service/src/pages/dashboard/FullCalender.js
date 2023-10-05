6a05df3591e36e0ba11a0213a96f260840fdbd7d
Sidharth-Sivaprasad <sksivapr@asu.edu>
2023-10-05 16:11:46 -0700

    Calender integration and styling  -2

--- Client_Service/src/pages/dashboard/FullCalender.js
+++ Client_Service/src/pages/dashboard/FullCalender.js
@@ -7,16 +7,19 @@ import dayGridPlugin from '@fullcalendar/daygrid'
   const calendarOptions = {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
-    height: '111%',
+    height: '135%',
     events: [
       { title: 'Event 1', date: '2023-10-10' },
       { title: 'Event 2', date: '2023-10-15' },
     ],
     headerToolbar: {
-        left: 'prev,next',
-        center: 'title',
-        right: 'today',
-      }
+        left: 'today',
+        center: 'prev,title,next',
+        right: '',
+      },
+    buttonText: {
+        today: 'Today', // Change the "today" button text to "Today"
+      },
   };
 
 
@@ -30,17 +33,11 @@ const Calendar = ({children, isOpen}) => {
     <div className="modal">
         <div className="popup">
             <div className="modalContent">
-            <div className="header">
-                <div className='pathTitle center'>
-                    Activity Calendar
-                </div>
-                <div onClick={handleClose} className='right'>
+            <div onClick={handleClose} className='right1'>
                     <img src='/icons/close.svg' style={{height: '30%'}}/>
                 </div> 
-            </div>
             <div>
                 <div className="fullcalendar-container">
-                    <br></br>
       <FullCalendar
         {...calendarOptions}
       />
