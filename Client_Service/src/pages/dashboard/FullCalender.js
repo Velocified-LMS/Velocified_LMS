1b46901c5750aeab738fc4e2e86ccb5425f7d5cc
Vardhan Reddy <vardhanreddy@Vardhans-Mac-mini.local>
2023-10-05 20:50:56 -0700

    cleaned up calendar

--- Client_Service/src/pages/dashboard/FullCalender.js
+++ Client_Service/src/pages/dashboard/FullCalender.js
@@ -3,51 +3,47 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
 
-
-  const calendarOptions = {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth',
-    height: '135%',
-    events: [
-      { title: 'Event 1', date: '2023-10-10' },
-      { title: 'Event 2', date: '2023-10-15' },
-    ],
-    headerToolbar: {
-        left: 'today',
-        center: 'prev,title,next',
-        right: '',
-      },
-    buttonText: {
-        today: 'Today', // Change the "today" button text to "Today"
-      },
-  };
-
-
 const Calendar = ({children, isOpen}) => {
 
     const handleClose = () => {
+      console.log('closed');
       isOpen(false);
     };
+
+    const calendarOptions = {
+      plugins: [dayGridPlugin],
+      initialView: 'dayGridMonth',
+      height: '135%',
+      events: [
+        { title: 'Event 1', date: '2023-10-10' },
+        { title: 'Event 2', date: '2023-10-15' },
+      ],
+      headerToolbar: {
+          left: 'today',
+          center: 'prev,title,next',
+          right: 'customButton',
+        },
+      buttonText: {
+          today: 'Today',
+        },
+      customButtons: {
+        customButton: {
+          click: handleClose
+        },
+      },
+    };
+
     const completion = 30;
     return (
-    <div className="modal">
-        <div className="popup">
-            <div className="modalContent">
-            <div onClick={handleClose} className='right1'>
-                    <img src='/icons/close.svg' style={{height: '30%'}}/>
-                </div> 
-            <div>
-                <div className="fullcalendar-container">
-      <FullCalendar
-        {...calendarOptions}
-      />
-    </div>
-
+      <div className="popup">
+          <div className="modal-content2">
+            <div className="fullcalendar-container">
+              <FullCalendar
+                {...calendarOptions}
+              />
             </div>
-            
-            </div>
-        </div>
-    </div>
+          </div>
+      </div>
     );
 };
 
