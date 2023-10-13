37eb7ead03967faf15a7c493074914d7977de703
Vashishtha Patel <vashishthapatel@Vashishthas-MacBook-Air.local>
2023-10-13 14:49:28 -0700

    SignUp

---
+++ Client_Service/src/pages/signup/page.js
@@ -0,0 +1,13 @@
+import styles from "./signup.css";
+import Navbar from "@/app/components/navbar";
+
+const SignUp = () => {
+  
+    return (
+      <div >
+        <Navbar />
+      </div>
+    );
+  };
+  
+  export default SignUp;
