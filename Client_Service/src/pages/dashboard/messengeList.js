2ce1589aac60b6df0cb715e9050a22a7c777701c
Vardhan Reddy <vardhanreddy@Vardhans-Mac-mini.local>
2023-10-13 20:14:50 -0700

    messages integrated

--- Client_Service/src/pages/dashboard/messengeList.js
+++ Client_Service/src/pages/dashboard/messengeList.js
@@ -1,58 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import styles from './messageList.module.css';
 
-function MessageList() {
-    const [messages, setMessages] = useState([]);
-
-    useEffect(() => {
-        // Fetch the messages from the JSON file (you may need to adjust the path)
-        fetch('/path-to-messages.json')
-            .then(response => response.json())
-            .then(data => setMessages(data))
-            .catch(error => console.error('Error fetching messages:', error));
-    }, []);
-
-    // Sort messages by timestamp in ascending order
-    const sortedMessages = messages.slice().sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+function MessageList({ messages }) {
 
     return (
         <div className={styles.message_list}>
-            {sortedMessages.map(message => (
+             {messages.map(message => (
                 <div
-                    key={message.id}
+                    key={message.sender.id}
                     className={`${styles.message} ${
-                        message.sender === 'User1' ? styles.message_left : styles.message_right
+                        message.sender.name === 'sai' ? styles.message_left : styles.message_right
                     }`}
                 >
+                    <div style={{ display: 'flex' }}>
+                        <div className={styles.message_text_with_border}>
+                            {message.content}
+                        </div>
+                    <div className={styles.message_container}>
+                    <div className={styles.sender_icon}>
+                        <img src="/Icon2.png" alt="User2 Icon" />
+                        <div className={styles.sender_name}>{message.sender.name}</div>
+                    </div>
+                    </div>
                     
-                        {message.sender === 'User1' ? (
-                            <div style={{ display: 'flex' }}>
-                                <div className={styles.message_container}>
-                                <div className={styles.sender_icon}>
-                                    <img src="/Icon1.svg" alt="User1 Icon" />
-                                    <div className={styles.sender_name}>{message.sender}</div>    
-                                </div>
-                                </div>
-                            <div className={styles.message_text_with_border}>
-                                {message.text}
-                            </div>
-                            </div>
-                        ) : (
-                            <div style={{ display: 'flex' }}>
-                                <div className={styles.message_text_with_border}>
-                                    {message.text}
-                                </div>
-                            <div className={styles.message_container}>
-                            <div className={styles.sender_icon}>
-                                <img src="/Icon2.svg" alt="User2 Icon" />
-                                <div className={styles.sender_name}>{message.sender}</div>
-                            </div>
-                            </div>
-                            
-                            </div>
-                        )}
-                    
-                    
+                    </div>
                 </div>
             ))}
         </div>
