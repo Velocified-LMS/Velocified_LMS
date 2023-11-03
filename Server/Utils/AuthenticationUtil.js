4f40ea07b344521409bc007fd1f899df9e4177a7
Vardhan Reddy <vardhanreddy@Vardhans-Mac-mini.local>
2023-11-02 22:41:40 -0700

    removed unneccessary logs

--- Server/Utils/AuthenticationUtil.js
+++ Server/Utils/AuthenticationUtil.js
@@ -1,6 +1,4 @@
 const isAuthenticated = (req, res, next) => {
-    console.log('Checking authentication...', req.session);
-
     if (!req.session.user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
